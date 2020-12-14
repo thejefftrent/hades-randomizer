768ea6077086006daf1474a5bc6fe0dc1252e35b
Jeff Trent <thejefftrent@gmail.com>
2020-12-14 15:28:10 -0700

    rebuild static website for github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'hades_randomizer',
+    title: 'Hades Randomizer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -43,5 +43,9 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+  },
+
+  generate: {
+    dir: 'docs'
   }
 }
